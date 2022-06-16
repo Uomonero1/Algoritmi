68141093bf12d18103b99ffdb5ee396667b57bbc
Uomonero1 <niks9498@gmail.com>
2022-06-16 18:30:18 +0200

    little commit

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -4,7 +4,7 @@ import numpy as np
 lista = np.random.randint(0, 1000, size=1000, dtype=int)
 
 def bubble_sort(lista):
-    for j in range(len(lista)):
+    for _ in range(len(lista)):
         for i in range(len(lista)-1):
             if lista[i] < lista[i+1]:
                 continue
